a6fef7844459c127c8c66ca9b90923c7dfbc6eaf
Jay <No2mad2@users.noreply.github.com>
2015-11-06 17:59:23 -0500

    Create FriendCatalog.py
    A "catalog" function, which archives an accounts recent tweets. Proof of concept

---
+++ FriendCatalog.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy, time, sys, json, re
+from tweepy import OAuthHandler
+from tweepy.api import API
+from sys import argv
+CONSUMER_KEY = 'xxx'
+CONSUMER_SECRET = 'xxx'
+ACCESS_KEY = 'xxx-xxx'
+ACCESS_SECRET = 'xxx'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)  
+#Authentication is out of the way
+c = tweepy.Cursor(api.search, q='searchterm', include_entities=True).items() #set cursor up
+
+# Catalog Function
+# Will slowly archive every tweet.
+# Might be dangerous on some users who have
+# very high tweet counts.
+# Bandwidth usage not calculated, but probably
+# negligible
+
+f = open('Text', 'a')
+
+def cata(wait = 10): #catalog function
+    tweet = c.next()
+    f.write('@%s Location: %s %s Client: %s Created: %s %s\n' % (tweet.user.screen_name, str(tweet.geo), tweet.user.profile_image_url, tweet.source, tweet.created_at, tweet.text))
+    wait = time.sleep(wait)
+    return;
+    
+while True:
+    try:
+        cata()
+    except:
+        pass
+    
