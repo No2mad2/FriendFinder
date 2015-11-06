d36b1bcf360f133376a4f9a92c758557d03a0e78
Jay <No2mad2@users.noreply.github.com>
2015-11-06 18:07:02 -0500

    Create SlowFavoriter.py
    Same as Favoriter, but this is much less spammy in regards to requests. Only favorites every one to two minutes.

---
+++ SlowFavoriter.py
@@ -0,0 +1,31 @@
+﻿import tweepy, random, time
+from random import randint
+
+auth = tweepy.OAuthHandler("xxx", "xxx")
+auth.set_access_token("xxx", "xxx")
+
+api = tweepy.API(auth)
+
+searches = api.search("searchterm", count=100)
+for tweet in searches:
+    timerand = (randint(60, 120))
+    time.sleep(timerand)
+    result = (randint(0,10))
+    print(result)
+    print (timerand)
+    #limits = api.rate_limit_status()
+    #remain_search_limits = limits['resources']['search']['/search/tweets']['remaining']
+    #print(remain_search_limits)
+    #print tweet.text.encode('utf8')
+    print "Preparing to favorite"
+    try:
+        print "tweet id %s" % (tweet.id)
+        api.create_favorite(tweet.id)
+        print "Successfully favorited!"
+    except:
+        pass
+
+    #if result == 1:
+        #api.retweet(tweet.id)
+        #print "Reweeted id %s" % (tweet.id)
+    
