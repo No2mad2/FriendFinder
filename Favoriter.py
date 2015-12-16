ba788479ad3bf0f3ef7b80ccccf8006686d8de0e
Jay <No2mad2@users.noreply.github.com>
2015-12-16 09:07:07 -0500

    Quickly favorites new tweets based on keyword
    Cleaned up code

--- Favoriter.py
+++ Favoriter.py
@@ -3,17 +3,12 @@ from random import randint
 
 auth = tweepy.OAuthHandler("xxx", "xxx")
 auth.set_access_token("xxx", "xxx")
-
 api = tweepy.API(auth)
 
-searches = api.search("searchterm", count=100) #Change searchterm
+searches = api.search("searchterm", count=100) #Change searchterm and number returned
 for tweet in searches:
     result = (randint(0,10))
     print(result)
-    limits = api.rate_limit_status()
-    remain_search_limits = limits['resources']['search']['/search/tweets']['remaining']
-    #print(remain_search_limits)
-    #print tweet.text.encode('utf8') - Might cause errors if emojis are printed
     print "Preparing to favorite"
     try:
         print "tweet id %s" % (tweet.id)
@@ -25,4 +20,3 @@ for tweet in searches:
     #if result == 1:
         #api.retweet(tweet.id)
         #print "Reweeted id %s" % (tweet.id)
-    # Remove comments for random retweets from new favorites
