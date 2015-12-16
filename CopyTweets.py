8ac172cca615bc460f58f979404fd57f99cc9b8a
Jay <No2mad2@users.noreply.github.com>
2015-12-16 09:05:57 -0500

    Copies the latest tweets from any public account
    Commented and cleared up code

--- CopyTweets.py
+++ CopyTweets.py
@@ -3,7 +3,7 @@
 #use timing function to tweet as soon as possible
 
 
-import tweepy, json, random, time, codecs
+import tweepy, random, time, codecs
 
 #tweepy auth
 from tweepy import OAuthHandler
@@ -18,19 +18,19 @@ api = tweepy.API(auth, compression=True)
 
 #===================================================
 
-filename = open('foo.txt')
-f = filename.readlines()
+filename = open('foo.txt') #Read account list
+f = filename.readlines() #One account name per line
 filename.close()
 
 def copytweet():
     randnum = random.randint(900, 3600)
-    for line in f:
+    for line in f: #find latest tweet in each account
         for status in tweepy.Cursor(api.user_timeline, id=line).items(1):
             print status.text
             print status.id
             tweetid = status.id
             print 'about to post status'
-            api.update_status(status.text)
+            api.update_status(status.text) #Copies latest tweet and moves on
             print 'posted status'
             print 'sleeping for %s' % (randnum)
             time.sleep(randnum)
